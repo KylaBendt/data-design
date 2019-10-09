f9bc72354ba64498d6758b3a973ff19670dd7c2b
Kyla Bendt <kylabendt@gmail.com>
2019-10-09 14:22:16 -0600

    complete conceptual model & ERD

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-		<meta charset="UTF-8">
-		<title>Conceptual Model - Kyla's Data Design Project</title>
+	<meta charset="UTF-8">
+	<title>Conceptual Model - Kyla's Data Design Project</title>
 </head>
 <body>
 	<h1>Kyla's Data Design Project - Conceptual Model</h1>
@@ -13,6 +13,34 @@
 		<a href="use-case.php"><button>Use Case/Interaction Flow</button></a>
 		<a href="conceptual-model.php"><button>Conceptual Model</button></a>
 	</nav>
+
+<h2>Entities & Attributes</h2>
+
+<h3>User</h3>
+<ul>
+	<li>userId (PK)</li>
+	<li>userActivationToken (Account verification)</li>
+	<li>userName</li>
+	<li>userEmail</li>
+	<li>userHash (Password)</li>
+</ul>
+
+<h3>Route</h3>
+<ul>
+	<li>routeId (PK)</li>
+	<li>routeUserId (FK)</li>
+	<li>routeName</li>
+	<li>routeDetails</li>
+	<li>routeCoordinates</li>
+</ul>
+
+<h2>Relations</h2>
+	<ul>
+		<li>One <strong>User</strong> can have multiple <strong>Routes</strong></li>
+	</ul>
+
+<h2>Entity Relationship Diagram</h2>
+	<img src="images/erd.jpg" alt="ERD">
 
 </body>
 
