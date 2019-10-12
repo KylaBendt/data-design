7653926877eb4fb71ca5a460b787c5e67c414f92
Kyla Bendt <kylabendt@gmail.com>
2019-10-12 06:39:00 -0600

    changed routeCoordinates and routeDetails types

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -18,9 +18,9 @@ create table users(
 create table routes(
 	routeId binary(16) not null,
 	routeUserId binary(16) not null,
-	routeCoordinates varchar(4000),
+	routeCoordinates linestring,
 	routeDateTime datetime(6),
-	routeDetails multipoint,
+	routeDetails varchar(2000),
 	routeDuration time,
 	foreign key(routeUserId) references users(userId),
 	primary key(routeId)
